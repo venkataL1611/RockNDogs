0b2dc7c32ee350a6a1dce665f60a8cb7c25857e6
raviteja <raviteja.lanka95@gmail.com>
2025-10-28 13:43:23 -0400

    fix: use ELASTICSEARCH_URL env var in ES connection

--- ElasticSearch/connection.js
+++ ElasticSearch/connection.js
@@ -1,6 +1,7 @@
 const { Client } = require('@elastic/elasticsearch');
 
-const client = new Client({ node: 'http://localhost:9200' });
+const esUrl = process.env.ELASTICSEARCH_URL || 'http://localhost:9200';
+const client = new Client({ node: esUrl });
 
 client.ping({}, (error) => {
   if (error) {
